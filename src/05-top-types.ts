70d4edea4761fbe78740138e61139a3b4198bdf6
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 16:29:12 +0530

    TE5-T158 added: top types

---
+++ src/05-top-types.ts
@@ -0,0 +1,25 @@
+// to run this file, type in this comman in 
+// command prompt => `ts-node {filename}.ts`
+
+// top types are the types which can hold any 
+// value. [any,unknown]
+
+let age:any = 35;
+
+// no error
+age = '35'
+
+// no error
+age = [1, 2, '3']
+
+console.log(age)
+
+// no error
+console.log(age.myAge)
+
+let Name: unknown = 'adarsh'
+
+console.log(Name)
+
+// throws error
+// Name.lastName
