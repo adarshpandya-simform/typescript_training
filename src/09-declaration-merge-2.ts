2c91560cefc63a96d720ed170802259e9d2e2be7
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 17:29:14 +0530

    TE5-T158 added: declaration merge function vs namespace

---
+++ src/09-declaration-merge-2.ts
@@ -0,0 +1,16 @@
+// to run this file, type in this comman in 
+// command prompt => `ts-node {filename}.ts`
+
+function format(amount: number): string{
+    return `${format.currency}${amount.toFixed(2)}`
+}
+
+namespace format{
+    export const currency:string = '$ '
+}
+
+// works
+console.log(format(5))
+
+// this also works
+console.log(format.currency)
